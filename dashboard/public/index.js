e305c6acda283ac703cfd1963e194240f8c5b67c
bigdaarpit <arpitjain0112@gmail.com>
2017-06-14 23:17:45 +0530

    start

---
+++ dashboard/public/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by arpit on 14/6/17.
+ */
+     console.log("aff")
+ /**   var a={228 : true,
+    32 : available,
+    65: available,
+    69 :available,
+     61: not_available,
+    229:available,
+    63 : available,
+     227 : available,
+    230 :available,
+     31 : available
+    }
+     console.log(a)
+**/
+ var a= {one:"able",32:false,owne:"able",322:false}
+ console.log(a)
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(person.age);
+console.log(a.one);
+var v=a.toJSON()
