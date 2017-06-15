4482808a9e2f377b3ce92766278047dee5c3d1e0
bigdaarpit <arpitjain0112@gmail.com>
2017-06-15 18:41:17 +0530

    input in object form

--- dashboard/public/index.js
+++ dashboard/public/index.js
@@ -1,23 +1,46 @@
 /**
  * Created by arpit on 14/6/17.
  */
-     console.log("aff")
- /**   var a={228 : true,
-    32 : available,
-    65: available,
-    69 :available,
-     61: not_available,
-    229:available,
-    63 : available,
-     227 : available,
-    230 :available,
-     31 : available
+
+window.onload = function() {
+    console.log('ds')
+
+
+    console.log("aff")
+
+
+    const data = `10.30.1.228 is available. 
+10.30.1.32 is available. 
+10.30.1.65 is available. 
+10.30.1.69 is available. 
+10.30.1.61 is not available. 
+10.30.1.229 is available. 
+10.30.1.63 is available. 
+10.30.1.227 is available. 
+10.30.1.230 is available. 
+10.30.1.31 is available. 
+203.176.113.41 is not available. 
+203.176.113.42 is not available.`
+
+    const stats = data.split('\n').map(line=> {
+        const [ip,verb,status] = line.split(' ')
+        return {ip, status}
+    });
+
+    console.log(stats);
+
+    var i = 0;
+
+    while (i < 10) {
+        if (stats[i].status != "available.") {
+            console.log(stats[i].status)
+            var el = document.getElementById("#\30'"+i);
+            console.log(el)
+            el.backgroundColor("black");
+
+        }
+        i++;
     }
-     console.log(a)
-**/
- var a= {one:"able",32:false,owne:"able",322:false}
- console.log(a)
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-console.log(person.age);
-console.log(a.one);
-var v=a.toJSON()
+};
+var p=document.getElementById("zero");
+console.log(p);
